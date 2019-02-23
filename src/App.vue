562dc2c193d2c38758f49fd75fc6feedc26d31bf
shubhambattoo <shubham.battoo@hotmail.com>
2019-02-23 14:17:03 +0530

    added navbar, removed init text

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+		<Navbar />
     <router-view/>
   </div>
 </template>
 
+<script>
+import Navbar from "@/components/Navbar";
+export default {
+	components : {
+		Navbar
+	}
+};
+</script>
+
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="navbar navbar-fixed">
+    <nav class="nav-extended deep-orange darken-2">
+      <div class="container">
+        <div class="nav-content">
+          <router-link to>
+            <span class="nav-title">theka-mixes</span>
+          </router-link>
+          <a class="btn-floating btn-large halfway-fab waves-effect waves-light amber accent-4">
+            <router-link to>
+              <i class="material-icons">add</i>
+            </router-link>
+          </a>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "navbar",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+</style>
+
